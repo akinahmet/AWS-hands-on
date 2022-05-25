ce30bdffb3128d5e97222fb4cb5461a06af6e6b4
AKN4 <ahmtakn@protonmail.com>
2022-05-25 15:30:17 +0200

    modified

--- ec2volumes/4-volume-mount.sh
+++ ec2volumes/4-volume-mount.sh
@@ -2,8 +2,11 @@
 
 
 # Back up the /etc/fstab file.
+
 sudo cp /etc/fstab /etc/fstab.bak
+
 # Open /etc/fstab file and add the following info to the existing.(UUID's can also be used)
+
 sudo nano /etc/fstab  # sudo vim /etc/fstab   >>> for vim
 
 /dev/xvdf       /mnt/2nd-vol        ext4    defaults,nofail        0       0
@@ -12,12 +15,19 @@ sudo nano /etc/fstab  # sudo vim /etc/fstab   >>> for vim
 
 # more info for fstab >> https://wiki.debian.org/fstab
 # Reboot and show that configuration exists (NOTE)
+
 sudo reboot now
+
 # List volumes to show current status, all volumes and partittions should be listed
+
 lsblk
+
 # Show the used and available capacities related with volumes and partitions
+
 df -h
+
 # Check if the data still persists.
+
 ls -lh /mnt/2nd-vol/
 ls -lh /mnt/3rd-vol-part2/
 
